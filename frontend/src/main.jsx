c34edb5d02a093f54b29ac5e087843529a2cd80a
unknownloop11 <unknownloop11@gmail.com>
2024-08-02 13:10:07 +0530

    Added analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
@@ -79,6 +80,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CookiesProvider>
       <MyProvider>
+        <Analytics />
         <RouterProvider router={router} />
       </MyProvider>
     </CookiesProvider>
