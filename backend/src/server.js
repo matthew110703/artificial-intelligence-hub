492cb76755b3e1d72e004840954672c73bf9e2b4
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-01-29 15:25:11 +0530

    Initialized MongoDB connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,12 @@
 import app from "./app.js";
+import dotenv from "dotenv";
+import connectDB from "./lib/connectDB.js";
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
 
 const PORT = process.env.PORT || 3001;
 
