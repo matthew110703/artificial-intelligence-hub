afb29a08929d15782d6d72c68b87783c170a3d1f
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-01-30 13:23:34 +0530

    Initialized node mailer

---
+++ backend/src/lib/emailConfig.js
@@ -0,0 +1,40 @@
+import { createTransport } from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const transporter = createTransport({
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+/**
+ *
+ * @param {string} to - Recipient's email address
+ * @param {string} otp - Unique OTP to be sent
+ */
+const sendMail = async (to, otp) => {
+  const info = await transporter.sendMail({
+    from: `"Artificial Intelligence Hub" <${process.env.EMAIL_USER}>`,
+    to,
+    subject: "OTP verification",
+    text: "please use the following OTP to verify your email",
+    html: `
+    <html>
+      <div>
+        <p>please use the following OTP to verify your email</p>
+        <h1>${otp}</h1>
+        <p>OTP is valid for 5 minutes</p>
+        <p>Do not share this OTP with anyone</p>
+      </div>
+    </html>
+    `,
+  });
+};
+
+export { sendMail };
