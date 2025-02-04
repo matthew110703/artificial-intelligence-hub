4e5c2cde407807b17f45f51b5e0dbe16dec30b8e
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-02-04 15:25:00 +0530

    Update emailConfig.js

--- backend/src/lib/emailConfig.js
+++ backend/src/lib/emailConfig.js
@@ -6,7 +6,6 @@ dotenv.config();
 const transporter = createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
-  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
