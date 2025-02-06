df6c08d52d969a6e6c03d918461fc3ebd02419a4
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-02-06 15:30:30 +0530

    Imported required assets

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,50 @@
+// Icons (Light-mode)
+import logo from "./logo.png";
+import account from "./icons/account.png";
+import audioWave from "./icons/audio-wave.png";
+import botPrimary from "./icons/bot-dark.png";
+import botSecondary from "./icons/bot.png";
+import darkMode from "./icons/dark-mode.png";
+import email from "./icons/email.png";
+import envelope from "./icons/envelope.png";
+import getStarted from "./icons/get-started.png";
+import info from "./icons/info.png";
+import login from "./icons/login.png";
+import office from "./icons/office.png";
+import personal from "./icons/personal.png";
+import password from "./icons/password.png";
+import reload from "./icons/reload.png";
+import rightArrow from "./icons/right-arrow.png";
+import roundedPlay from "./icons/rounded-play.png";
+import school from "./icons/school.png";
+import sourceCode from "./icons/source-code.png";
+import sparks from "./icons/sparks.png";
+import username from "./icons/username.png";
+import upArrow from "./icons/up-arrow.png";
+import lightMode from "./icons/light-mode.png";
+
+export {
+  logo,
+  account,
+  audioWave,
+  botPrimary,
+  botSecondary,
+  darkMode,
+  email,
+  envelope,
+  getStarted,
+  info,
+  login,
+  office,
+  personal,
+  password,
+  reload,
+  rightArrow,
+  roundedPlay,
+  school,
+  sourceCode,
+  sparks,
+  username,
+  upArrow,
+  lightMode,
+};
