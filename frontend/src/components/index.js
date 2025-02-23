364642088e603370931919ae7324c36adecfee0f
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-02-23 13:14:34 +0530

    Initialized central export file fot components

---
+++ frontend/src/components/index.js
@@ -0,0 +1,38 @@
+// Layout Components
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+import Sidebar from "./layout/Sidebar";
+import Container from "./layout/Container";
+
+export { Header, Footer, Sidebar, Container };
+
+// UI Components (Reusable)
+import Icon from "./ui/Icon";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import NavLink from "./ui/NavLink";
+import ToolCard from "./ui/Cards/ToolCard";
+import PurposeCard from "./ui/Cards/PurposeCard";
+import Hero from "./ui/Hero";
+import Feature from "./ui/Feature";
+import ImageContainer from "./ui/ImageContainer";
+import Badge from "./ui/Badge";
+
+export {
+  Icon,
+  Button,
+  Input,
+  NavLink,
+  ToolCard,
+  PurposeCard,
+  Hero,
+  Feature,
+  ImageContainer,
+  Badge,
+};
+
+// Forms
+import SignUpForm from "./forms/SignUpForm";
+import LoginForm from "./forms/LoginForm";
+
+export { SignUpForm, LoginForm };
